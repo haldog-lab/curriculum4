979073c867c829bc736324300e4197a3ff67b951
haldog-lab <ackey17@gmail.com>
2022-11-16 18:13:04 +0900

    Update index2.php

--- PHP-job/ex/index2.php
+++ PHP-job/ex/index2.php
@@ -4,11 +4,11 @@ $pw = "pass";
 
 if ($name = "taro" && $pw = "pass") {
     echo 'ログイン成功です ';
-} elseif ($name != "taro") && ($pw = "pass"){
+} elseif ($name != "taro" && $pw = "pass"){
     echo '名前が間違っています。';
-} elseif ($name = "taro") && ($pw != "pass"){
+} elseif ($name = "taro" && $pw != "pass"){
     echo 'パスワードが間違っています。';
-}elseif ($name != "taro") && ($pw != "pass"){
+}elseif ($name != "taro" && $pw != "pass"){
     echo '入力情報が間違っています。';
 }
 ?>
