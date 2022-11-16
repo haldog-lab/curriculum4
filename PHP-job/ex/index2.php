d92c76a1d28bd8f0514116bc4ce005ddc9a443d3
haldog-lab <ackey17@gmail.com>
2022-11-16 17:25:56 +0900

    Update index2.php

--- PHP-job/ex/index2.php
+++ PHP-job/ex/index2.php
@@ -1,9 +1,10 @@
 <?php
-$age = 18;
+// 年齢
+$age = 24;
+// 学生かどうか
+$is_student = true;
 
-if ($age >= 20) {
-    echo "お酒が飲めるぞ！";
-} else {
-    echo "お酒は二十歳になってから！";
+if ($age < 25 && $is_student) {
+    echo '学割パックが使えるよ';
 }
 ?>
