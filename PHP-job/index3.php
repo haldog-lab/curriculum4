cb4810b01281a7915a0e83fb12ddeab57931859e
haldog-lab <ackey17@gmail.com>
2022-11-17 17:52:33 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -1,17 +1,10 @@
 <?php
-$name = "tao";
-$pw = "ps";
-
-if ($name == "taro" && $pw == "pass") {
-    echo 'ログイン成功です ';
-}
-elseif ($name != "taro" && $pw == "pass"){
-    echo '名前が間違っています';
-}
-elseif ($name == "taro" && $pw != "pass"){
-    echo 'パスワードが間違っています';
-}
-elseif ($name != "taro" && $pw != "pass"){
-    echo '入力情報が間違っています。';
+$num = 0;
+while($num < 10) {
+    echo $num;
+    $num++;
+    if($num == 5) {
+        break;
+    }
 }
 ?>
