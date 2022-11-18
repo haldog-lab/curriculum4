0973385c51fee6fb4aee867b7062e6c60360d921
haldog-lab <ackey17@gmail.com>
2022-11-18 11:10:07 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -1,12 +1,13 @@
 <?php
 $fruits = ["apple" => "りんご", "orange" => "みかん", "grape" => "ぶどう"];
-
+$fruits["peach"] = "もも";
 // りんごを出力
 echo $fruits["apple"];
 // みかんを出力
 echo $fruits["orange"];
 // ぶどうを出力
 echo $fruits["grape"];
+echo $fruits["peach"];
 
 var_dump($fruits);
 ?>
