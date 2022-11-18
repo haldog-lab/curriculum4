62b9af89b0cb2ce551edf77e23e4ac9f23a30db0
haldog-lab <ackey17@gmail.com>
2022-11-18 11:52:13 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -4,7 +4,8 @@ var_dump($colors);
 echo $colors["red"];
 echo $colors["blue"];
 echo $colors["green"];
-
+?>
+<?php
 $colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
 $colors = ["yellow" => "黄色"];
 var_dump($colors);
