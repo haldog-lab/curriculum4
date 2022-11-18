99127160084ac650e634dc6f33225bd981b61ef0
haldog-lab <ackey17@gmail.com>
2022-11-18 11:01:35 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -1,8 +1,15 @@
 <?php
-$countries = ["America", "Japan", "China", "Korea"];
-var_dump($countries);
-echo $countries[0];
-echo $countries[1];
-echo $countries[2];
-echo $countries[3];
+$fruits = ["りんご", "みかん", "ぶどう"];
+
+// 0番目のりんごを出力
+echo $fruits[0];
+echo `<br />`;
+// 1番目のみかんを出力
+echo $fruits[1];
+echo `<br />`;
+// 2番目のぶどうを出力
+echo $fruits[2];
+echo `<br />`;
+// 配列全体を出力
+var_dump($fruits);
 ?>
