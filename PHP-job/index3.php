8e77b0bfac4de28470ddafef1775b6408da5416a
haldog-lab <ackey17@gmail.com>
2022-11-18 11:04:29 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -3,13 +3,13 @@ $fruits = ["りんご", "みかん", "ぶどう"];
 
 // 0番目のりんごを出力
 echo $fruits[0];
-echo `<br />`;
+echo <br>;
 // 1番目のみかんを出力
 echo $fruits[1];
-echo `<br />`;
+echo <br>;
 // 2番目のぶどうを出力
 echo $fruits[2];
-echo `<br />`;
+echo <br>;
 // 配列全体を出力
 var_dump($fruits);
 ?>
