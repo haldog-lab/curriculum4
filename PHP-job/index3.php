a36cbee662ce82d69df829f8ff3d254c7ed5caa4
haldog-lab <ackey17@gmail.com>
2022-11-18 16:01:51 +0900

    Update index3.php

--- PHP-job/index3.php
+++ PHP-job/index3.php
@@ -1,9 +1,13 @@
 <?php
-$fruits = ["apple" =>"といったらりんご", "orange" => "といったらみかん", "peach" => "といったらもも"];
-
-foreach ($fruits as $key => $value) {
-    echo $key;
-    echo $value;
-    echo '<br>';
+function getTriangleArea($base, $height) {
+    $area = $base * $height / 2;
+    print "三角形の面積は".$area."だよ。";
 }
+
+// 1)
+getTriangleArea(10,5);
+// 2)
+getTriangleArea(15,8);
+// 3)
+getTriangleArea(8,6);
 ?>
