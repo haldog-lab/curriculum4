d9f08f6f96f45493ab19d707ec1fb2f78cb56107
haldog-lab <ackey17@gmail.com>
2022-11-30 11:24:54 +0900

    Update index5.php

--- PHP-job/3-2/index5.php
+++ PHP-job/3-2/index5.php
@@ -1,14 +1,14 @@
 <?php
 $fruits = ["apple" => 3, "orange" => 3, "peach" => 5];
 
-function totalcost($fruits,$npiece) {
+function total($fruits,$npiece) {
    $total = $fruits * $npiece ;
    print "りんごは $total 円です。"
-   print </br>
+   echo `<br />`;
    print "りんごは $total 円です。"
-   print </br>
+   echo `<br />`;
    print "りんごは $total 円です。"
-   print </br>
+   echo `<br />`;
 
    
    $num = 0; 
