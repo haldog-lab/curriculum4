b99c6a1d9459dab610733a8779e596fab0e9d306
haldog-lab <ackey17@gmail.com>
2022-12-05 17:14:47 +0900

    Update index5.php

--- PHP-job/3-2/index5.php
+++ PHP-job/3-2/index5.php
@@ -1,20 +1,20 @@
 <?php
 $fruits = ["apple" => 100, "orange" => 50, "peach" => 600];
 
-function total($fruits,$num) {
-   $total = $fruits * $num ;
-   print "りんごは .$total. 円です。"
+function getprice($fruits,$num) {
+   $getprice = $fruits * $num ;
+   print "りんごは .$getprice. 円です。"
    echo `<br />`;
-   print "りんごは .$total. 円です。"
+   print "りんごは .$getprice. 円です。"
    echo `<br />`;
-   print "りんごは .$total. 円です。"
+   print "りんごは .$getprice. 円です。"
    echo `<br />`;
 
    
-   $num = 0; 
-   while($num <= 2) {
-       echo $num;
-       $num++;
+   $fruits = 0; 
+   while($fruits <= 2) {
+       echo $fruits;
+       $fruits++;
    }
 
 }
