e54327c9127af99461ba00657ed21ec8191e765a
haldog-lab <ackey17@gmail.com>
2022-12-09 16:35:34 +0900

    Update index5.php

--- PHP-job/3-2/index5.php
+++ PHP-job/3-2/index5.php
@@ -1,10 +1,13 @@
 <?php
 $fruits = ["apple" => 100, "orange" => 50, "peach" => 600];
 $num = [3,3,5];
-function getprice($fruits,$num) {
+function $getprice ($fruits,$num){
    $getprice = $fruits * $num ;
    return $getpreice;
-  
-
 }
+   foreach (fruits as $key => $value) {
+
+    echo $key."は".getPrice($value,$num[i]).“円です。”;
+    $i++;
+    }  
 ?>
