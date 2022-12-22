46b39a85eb2568566ba31c0d709d2eeb3d8a25bb
haldog-lab <ackey17@gmail.com>
2022-12-22 18:33:23 +0900

    Update index5.php

--- PHP-job/3-2/index5.php
+++ PHP-job/3-2/index5.php
@@ -1,11 +1,13 @@
 <?php
 $fruits = ["apple" => 100, "orange" => 50, "peach" => 600];
 $num = [3,3,5];
-function getprice(fruits,num){
+function getprice($fruits,$num){
    $getprice = $fruits * $num ;
 
    return $getprice;
 }
+
+$fruits = 0
 foreach ($fruits as $key => $value) {
 
     echo $key."は".$getprice($value,$num[i])."円です。";
