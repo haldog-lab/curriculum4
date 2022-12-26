0d7321f0f4d0993767fc423ddc02a991464e73a6
haldog-lab <ackey17@gmail.com>
2022-12-26 17:38:58 +0900

    Update index5.php

--- PHP-job/3-2/index5.php
+++ PHP-job/3-2/index5.php
@@ -2,9 +2,9 @@
 $fruits = ["apple" => 100, "orange" => 50, "peach" => 600];
 $num = [3,3,5];
 function getprice($fruits,$num){
-   getprice = $fruits * $num ;
+   $getprice = $fruits * $num ;
 
-   return getprice;
+   return $getprice;
 }
 
 $i = 0;
